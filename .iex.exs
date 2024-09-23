aa2c63901f69344c685c3b6f233865aad863d9c1
Moosieus <cameron.duley99@gmail.com>
2024-09-23 18:14:22 -0400

    Update iex helpers and deps

--- .iex.exs
+++ .iex.exs
@@ -2,3 +2,4 @@ import Ecto.Query
 
 alias PdbDemo.Repo
 alias PdbDemo.Scanner.Call
+alias PdbDemo.Scanner.TalkGroup
--- mix.exs
+++ mix.exs
@@ -28,8 +28,8 @@ defmodule PdbDemo.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:ecto, path: "/home/moosieus/Projects/moo-ecto", override: true},
-      {:ecto_sql, path: "/home/moosieus/Projects/ecto_sql", override: true},
+      {:ecto, git: "https://github.com/moosieus/ecto", ref: "cf5d838f7fe1cb5da2044ccd3d9bf5af67eaac21", override: true},
+      {:ecto_sql, git: "https://github.com/moosieus/ecto_sql", ref: "fbf3f19e1a26bcb1ce55c5f60adbce6bc9bc69a5", override: true},
       {:postgrex, ">= 0.0.0"},
       {:ecto_map_set, path: "/home/moosieus/Projects/ecto_map_set"},
       {:jason, "~> 1.2"}
